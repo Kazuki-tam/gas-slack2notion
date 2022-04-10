acdb36457989a9d5b1e13c392e33cebb05ac76ff
yonemoto <yonemoto@tam-tam.co.jp>
2022-04-10 19:35:15 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { createItem } from "./helper/createItem";
+import { postNotion } from "./helper/postNotion";
+import { createTagsArray } from "./helper/createTagsArray";
+
 /**
  * main function
  */
-function main(){
-  console.log("Main function!!")
+function main() {
+  const mySheet = SpreadsheetApp.getActiveSheet();
+  const latestEntryRow = mySheet.getLastRow();
+  const question = mySheet.getRange(latestEntryRow, 1).getValue();
+  const answer = mySheet.getRange(latestEntryRow, 2).getValue();
+  const url = mySheet.getRange(latestEntryRow, 3).getValue();
+  const tags = createTagsArray(mySheet.getRange(latestEntryRow, 4).getValue());
+  // Get payload
+  const payload = createItem(question, answer, url, tags);
+  // Post messages to Notion
+  postNotion(payload);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
