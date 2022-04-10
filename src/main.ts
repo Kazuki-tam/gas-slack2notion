9251b45bf6b844f204aef44f7a0e97382ec03857
yonemoto <yonemoto@tam-tam.co.jp>
2022-04-10 20:09:27 +0900

    Update: url error

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,18 @@ import { createTagsArray } from "./helper/createTagsArray";
 function main() {
   const mySheet = SpreadsheetApp.getActiveSheet();
   const latestEntryRow = mySheet.getLastRow();
-  const question = mySheet.getRange(latestEntryRow, 1).getValue();
-  const answer = mySheet.getRange(latestEntryRow, 2).getValue();
-  const url = mySheet.getRange(latestEntryRow, 3).getValue();
-  const tags = createTagsArray(mySheet.getRange(latestEntryRow, 4).getValue());
-  // Get payload
-  const payload = createItem(question, answer, url, tags);
-  // Post messages to Notion
-  postNotion(payload);
+  if (latestEntryRow > 1) {
+    const question = mySheet.getRange(latestEntryRow, 1).getValue();
+    const answer = mySheet.getRange(latestEntryRow, 2).getValue();
+    const url = mySheet.getRange(latestEntryRow, 3).getValue() || null;
+    const tags = createTagsArray(
+      mySheet.getRange(latestEntryRow, 4).getValue()
+    );
+    // Get payload
+    const payload = createItem(question, answer, url, tags);
+    // Post messages to Notion
+    postNotion(payload);
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
